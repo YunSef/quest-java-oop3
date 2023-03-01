7939be5ed39a237825dfe29b649b94a7d41c76dd
yunsef <sebblonski77@gmail.com>
2023-03-01 11:28:03 +0100

    first commit

---
+++ Fly.java
@@ -0,0 +1,13 @@
+public interface Fly {
+	
+	//take off
+	void takeOff();
+	//ascend
+	void ascend(int meters);
+	//glide
+	void glide();
+	//descend
+	void descend(int meters);
+	//land
+	void land();
+}
